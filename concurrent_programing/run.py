a5afb0183a2f8daecea0ad3f7682d8b658e3a7ac
Diego Peres <speres.diego@gmail.com>
2018-10-12 11:06:58 -0300

    Refactoring code to use a generic life cycle

--- concurrent_programing/run.py
+++ concurrent_programing/run.py
@@ -1,15 +1,20 @@
 import socket
 
+INITIAL_STATE = 0
+SERVER_ACCEPT_STATE = 1
+REQUEST_READING_STATE = 1
+REQUEST_READ_STATE = 2
+REQUEST_READY_STATE = 3
+REQUEST_CLOSED_STATE = 4
+
 
 class IOLoop:
 
     _generators = []
 
     @classmethod
-    def schedule(cls, _callable):
-        def _create_generator(_callable):
-            yield _callable()
-        cls._generators.append(_create_generator(_callable))
+    def schedule(cls, generator):
+        cls._generators.append(generator)
 
     @classmethod
     def run(cls):
@@ -20,76 +25,94 @@ class IOLoop:
                 _generator.send(None)
 
 
-class Server:
-    def __init__(self, host, port):
-        print('Initializing asynchronous HTTP server')
+class LifeCycle:
+    @property
+    def cycles(self):
+        raise NotImplemented()
+
+    @property
+    def state(self):
+        return self._state
+
+    def __init__(self, *args):
+        self._state = INITIAL_STATE
+        self.initialize(*args)
+
+    def start(self):
+        self._cycles_length = len(self.cycles)
+        IOLoop.schedule(self._next_cycle())
+
+    def _next_cycle(self):
+        if self._cycles_length > self.state:
+            self._state = self.cycles[self.state]()
+            IOLoop.schedule(self._next_cycle())
+        yield self.state
+
+
+class Server(LifeCycle):
+    @property
+    def cycles(self):
+        return [self._accept, self._accept]
+
+    def initialize(self, host, port):
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self._socket.bind((host, port))
         self._socket.listen()
         self._socket.setblocking(False)
-        IOLoop.schedule(lambda : print('Ready!'))
-        IOLoop.schedule(self._accept)
         print('Listening host {0} on {1}'.format(host, port))
+
+    def start(self):
+        print('Initializing asynchronous HTTP server')
+        super().start()
 
     def _accept(self):
         try:
             connection, _ = self._socket.accept()
             connection.setblocking(False)
-            Request(connection)
+            Request(connection).start()
         except BlockingIOError:
             pass
-        IOLoop.schedule(self._accept)
+        return SERVER_ACCEPT_STATE
 
 
-class Request:
+class Request(LifeCycle):
 
     BUFFER_SIZE = 32
-    STATUS_INITIAL = 0
-    STATUS_READ = 1
-    STATUS_READY = 2
-    STATUS_CLOSED = 2
 
-    def __init__(self, connection):
-        self.data = b''
+    @property
+    def cycles(self):
+        return [self.read, self.read, self.ready, self.close]
+
+    def initialize(self, connection):
         self.connection = connection
-        self._status = self.STATUS_INITIAL
-        IOLoop.schedule(self.life_cycle)
+        self.data = b''
 
-    def life_cycle(self):
-        if self._status == self.STATUS_INITIAL:
-            self._read()
-        elif self._status == self.STATUS_READ:
-            self._ready()
-        elif self._status == self.STATUS_READY:
-            self._close()
-
-    def _read(self):
+    def read(self):
         try:
-            self._status = self.STATUS_READ
             self.data += self.connection.recv(self.BUFFER_SIZE)
-            IOLoop.schedule(self._read)
         except BlockingIOError:
-            IOLoop.schedule(self.life_cycle)
+            if self.state == REQUEST_READING_STATE:
+                return REQUEST_READ_STATE
+        return REQUEST_READING_STATE
 
-    def _ready(self):
-        self._status = self.STATUS_READY
-        IOLoop.schedule(self.life_cycle)
+    def ready(self):
+        self._send_response()
+        return REQUEST_READY_STATE
 
-    def _close(self):
-        self._status = self.STATUS_CLOSED
-        self.connection.send(self._get_response())
+    def close(self):
         self.connection.close()
+        return REQUEST_CLOSED_STATE
 
-    def _get_response(self):
+    def _send_response(self):
         response = [
             b'HTTP/1.1 200 OK',
             b'Content-Type: text/html; charset=utf-8',
             b'Content-Length: 0',
             b'Connection: Closed',
         ]
-        return b'\r\n'.join(response) + b'\r\n\r\n'
+        self.connection.send(b'\r\n'.join(response) + b'\r\n\r\n')
 
 
-server = Server('127.0.0.1', 8080)
+Server('127.0.0.1', 8080).start()
 IOLoop.run()
